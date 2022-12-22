18a5a1b435e0f155ab13ad3360e08628162f3f33
Iryna Brodnikovska <iryna_brodnikovska@epam.com>
2022-12-22 07:13:12 +0000

    UITesting

--- src/main/java/pageobjects/LaunchesPage.java
+++ src/main/java/pageobjects/LaunchesPage.java
@@ -1,21 +1,22 @@
 package pageobjects;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
-import entities.LaunchesMenus;
 import io.qameta.allure.Step;
 import lombok.Getter;
 import org.openqa.selenium.support.FindBy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testng.Assert;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 
 @Getter
-public class LaunchesPage {
+public class LaunchesPage extends BasePage{
     private static Logger logger = LoggerFactory.getLogger(LaunchesPage.class);
 
     @FindBy(xpath = "//span[contains(@class,'headerCell__title-full')]")
@@ -26,6 +27,9 @@ public class LaunchesPage {
 
     @Step
     public List<String> getLaunchesMenuTitles() {
+        getTheFullScreen();
+        Selenide.screenshot(Long. toString(System.currentTimeMillis()));
+        menus.get(0).shouldBe(Condition.exist, Duration.ofMillis(12000));
         return menus.stream()
                 .filter(e -> !e.text().isEmpty())
                 .map(SelenideElement::getText)
@@ -34,11 +38,12 @@ public class LaunchesPage {
 
     @Step
     public LaunchesPage openRandomTestResults() {
+        testRuns.get(0).shouldBe(Condition.visible, Duration.ofMillis(15000));
         int numberOfLaunches = testRuns.size();
         Random random = new Random();
         try {
             int randomNumberFromTheTop = random.nextInt(numberOfLaunches);
-            testRuns.get(randomNumberFromTheTop).click();
+            clickUsingJS(testRuns.get(randomNumberFromTheTop));
         } catch (IllegalArgumentException e) {
             e.toString();
         }
