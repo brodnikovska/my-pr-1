8edb11dc1391be8a5ff37f337ec4b25a2ec3293f
Brodnikovska <brodni01@kits.kfplc.com>
2022-12-30 00:48:26 +0200

    Add configuration for Selenium Grid

--- src/test/java/ui/BaseTestCase.java
+++ src/test/java/ui/BaseTestCase.java
@@ -4,12 +4,12 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
 import com.epam.reportportal.testng.ReportPortalTestNGListener;
 import lombok.SneakyThrows;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import pageobjects.LoginPage;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import utils.PropertyController;
 import org.testng.annotations.*;
 
+import java.net.URL;
 import java.time.Duration;
 
 import static com.codeborne.selenide.Selenide.open;
@@ -17,9 +17,14 @@ import static com.codeborne.selenide.Selenide.open;
 @Listeners({ReportPortalTestNGListener.class})
 public abstract class BaseTestCase {
 
+    @SneakyThrows
     @BeforeMethod
     public void before() {
-        Configuration.browser = PropertyController.getPropertyByKey("driver");
+        String urlToRemoteWD = "http://localhost:4444/wd/hub";
+        //ChromeOptions options = new ChromeOptions();
+        EdgeOptions options = new EdgeOptions();
+        RemoteWebDriver driver = new RemoteWebDriver(new URL(urlToRemoteWD), options);
+        WebDriverRunner.setWebDriver(driver);
         open(PropertyController.getPropertyByKey("base.url"));
         WebDriverRunner.getWebDriver().manage().timeouts().implicitlyWait(Duration.ofMillis(3000));
         Configuration.reportsFolder = "target/selenide-reports";
